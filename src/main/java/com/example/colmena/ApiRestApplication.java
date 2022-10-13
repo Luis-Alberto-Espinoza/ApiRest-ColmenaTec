550874c312dd50df1d815bdbf6ae2282917291d9
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2022-10-13 10:53:09 -0300

    ColmenaTec hasta video 7

--- src/main/java/com/example/colmena/ApiRestApplication.java
+++ src/main/java/com/example/colmena/ApiRestApplication.java
@@ -9,7 +9,7 @@ public class ApiRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 		//conectarse al php myadmin http://localhost/phpmyadmin
-
+		//http://localhost:9000/api/v1/personas
 	}
 
 }
